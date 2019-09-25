7f47d5c32c327f926903d11ca139ecc466a11f9d
Balaji751 <balajisri233@msitprogram.net>
2019-09-25 23:01:29 +0530

    day 3

---
+++ Module-3/Assignment 3/Squares.java
@@ -0,0 +1,42 @@
+/**
+ * Given an array of Integers, return an array of the square
+ * of each values in the array. 
+ * 
+ * Input : {1, 2, 3}
+ * Output : {1, 4, 9}
+ * 
+ * @author Siva Sankar
+ * @author Mayank
+ */
+import java.util.Scanner;
+
+public class Squares {
+
+    /**
+     * This method returns the array of ints that are squares of each element
+     * in the input array.
+     * 
+     * @param nums, the input array.
+     * 
+     * @return the array of ints that are squares of each element
+     * in the input array.
+     * 
+     */
+    public static int[] squared(int[] nums) {
+        //  Your code goes here.
+        Scanner s = new Scanner(System.in);
+        int [] arr= new int[nums.length];
+
+            for(int i=0;i<nums.length;i++){
+                arr[i]=nums[i]*nums[i];
+
+            }
+        
+        return arr;
+
+        
+
+        
+        
+    }
+}
