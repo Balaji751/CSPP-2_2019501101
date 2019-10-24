741067cce603e1129501336e8569b0e7db90b368
Balaji751 <balajisri233@msitprogram.net>
2019-10-24 13:36:02 +0530

    Done Clock Adt

--- Book Your Show/Patron.java
+++ Book Your Show/Patron.java
@@ -5,7 +5,7 @@ class Patron{
     Patron(String name,String number){
         this.name=name;
         this.mobileNumber=number;
-    }
+    } 
     public void setTicket(String t){
         this.ticket = t;
     }
